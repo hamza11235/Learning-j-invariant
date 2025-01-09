06681cd9df7ab027047e9e3017d7a1f836367caa
StringyHamza <74888860+hamza11235@users.noreply.github.com>
2025-01-09 16:08:31 -0500

    scripts added

---
+++ src/scripts/Feedforward.py
@@ -0,0 +1,21 @@
+from utils import *
+
+
+def feedforward(stepsize):
+    
+    """
+    Creating a feed-forward NN and initializing the parameters
+    """
+    
+    compRelu=elementwise(comprelu)
+    net_init, net_apply = stax.serial(
+        Dense1(256), compRelu,
+        Dense1(256), compRelu,
+        Dense1(256), Flatten,
+        Dense1(256), compRelu,
+        Dense1(1)
+    )
+    net_params, opt_init, opt_update, get_params=initparams(net_init,stepsize)
+    return net_params, opt_init, opt_update, get_params,net_apply
+
+
---
+++ src/scripts/Test.py
@@ -0,0 +1,21 @@
+from utils import *
+from Dataset import*
+
+
+
+def Test(learned_params,net_apply, batch_size, n, lamb):
+    
+    """
+    Testing learned parameters on unknown data
+    """
+    
+    
+    a1,b1,c1=gen(batch_size,n,lamb)
+    xs=net_apply(learned_params,b1)
+    ys=net_apply(learned_params, a1)
+    y=percentageloss(ys,xs)
+    x=np.arange(len(y))
+    plt.scatter(x, y)
+    plt.xlabel('Points')
+    plt.ylabel('Percentage difference')
+    plt.show()
